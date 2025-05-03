70da00312c0ce3bdec4675ee08374d982d699c94
Harnoorsoftware <harnoorsoftware@gmail.com>
2025-05-03 19:09:32 +0530

    Update Exercise_7_1.py

--- Exercise_7_1.py
+++ Exercise_7_1.py
@@ -1,1 +1,20 @@
 
+# 1. Find the ratio of the following.
+# (a) Speed of a cycle 15 km per hour to the speed of scooter 30 km per hour.
+# (b) 5 m to 10 km (c) 50 paise to  5
+
+Cycle_speed=15
+Scooter_speed=30
+
+a=Cycle_speed/Scooter_speed
+
+print("(a)=",a)
+
+#==========================
+
+#_______Variable define
+
+
+#_______Formula
+
+#===========================
